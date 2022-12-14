b2659fc7fee36098950d57d5687ed2648f3ff7be
Tim Kolberger <tim@kolberger.eu>
2022-12-14 18:19:53 +0100

    fix: esm export picked up as cjs (#108)
    see https://publint.bjornlu.com/@polymorphic-factory/react@0.2.1

--- packages/solid/tsup.config.ts
+++ packages/solid/tsup.config.ts
@@ -5,8 +5,5 @@ export default defineConfig({
   clean: true,
   format: ['cjs', 'esm'],
   dts: true,
-  outExtension(ctx) {
-    return { js: `.${ctx.format}.js` }
-  },
   esbuildPlugins: [solidPlugin()],
 })
--- packages/vue/tsup.config.ts
+++ packages/vue/tsup.config.ts
@@ -4,8 +4,6 @@ export default defineConfig({
   clean: true,
   format: ['cjs', 'esm'],
   dts: true,
-  outExtension(ctx) {
-    return { js: `.${ctx.format}.js` }
-  },
+
   inject: ['./src/jsx-inject.ts'],
 })
